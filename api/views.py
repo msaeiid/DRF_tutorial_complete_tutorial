8d881140dde95773070cd51de9a10a12000309ae
msaeiid <mskarbaschian@gmail.com>
2024-03-06 14:45:54 +0330

    47:30

--- api/views.py
+++ api/views.py
@@ -1,7 +1,17 @@
 from django.http import JsonResponse
 from rest_framework.request import Request
+from rest_framework.utils import json
 
 
 # building a simple API with JsonResponse
 def api_home(request: Request):
+    body = request.body
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(request.GET)
+    data['headers'] = dict(request.headers)
+    # data['content_type'] = request.content_type
+    print(data)
     return JsonResponse({"message": "Hi there, this is your Django API response"})
