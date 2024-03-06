b9ea44bc90be8c6be18238e97b8b63bda3b6047d
msaeiid <mskarbaschian@gmail.com>
2024-03-07 00:27:07 +0330

    Rest Framework View & Response

--- api/views.py
+++ api/views.py
@@ -1,15 +1,20 @@
 from django.forms import model_to_dict
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
 from rest_framework.request import Request
-from rest_framework.utils import json
+from rest_framework.response import Response
 
 from products.models import Product
 
 
 # building a simple API with JsonResponse
+@api_view(http_method_names=['GET'])
 def api_home(request: Request):
+    """
+    DRF API View
+    :param request:
+    :return: a random product will return
+    """
     product = Product.objects.all().order_by("?").first()
     data = model_to_dict(product, fields=["id", "title", "price"])
-    # json_data = json.dumps(data)
-    return JsonResponse(data)
-    # return HttpResponse(json_data, content_type="application/json")
+    return Response(data)
