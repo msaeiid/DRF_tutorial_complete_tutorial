a79e662ad4148e3052b79e9b5224c6b1835419a1
msaeiid <mskarbaschian@gmail.com>
2024-03-07 14:23:46 +0330

    Using Function Based Views For Create Retrieve or List

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,8 @@ from django.urls.conf import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='detail_view'),
-    path('', views.ProductListCreateAPIViews.as_view(), name='create_view'),
+    # path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='detail_view'),
+    # path('', views.ProductListCreateAPIViews.as_view(), name='create_view'),
+    path('', views.product_alt_view, name='product_alt_view'),
+    path('<int:pk>/', views.product_alt_view, name='product_alt_view'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
-from rest_framework import generics
-from .serializers import ProductSerializer
+from django.shortcuts import get_list_or_404, get_object_or_404
+from rest_framework import generics, views, status
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
 
+from .serializers import ProductSerializer
 from products.models import Product
 
 
@@ -23,3 +27,27 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'pk'
+
+
+@api_view(http_method_names=['POST', 'GET'])
+def product_alt_view(request: Request, pk=None, *args, **kwargs):
+    if request.method == "GET":
+        products = get_list_or_404(Product)
+        if pk:
+            product = get_object_or_404(Product, pk=pk)
+            serializer = ProductSerializer(product, many=False)
+        else:
+            serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            title = serializer.validated_data['title']
+            content = serializer.validated_data['content']
+            if content:
+                content = title
+                serializer.save(content=content)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # this is why generic views are good
