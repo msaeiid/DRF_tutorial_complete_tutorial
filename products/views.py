502e6249d6ffb1ae0861923e22eeb050e7858c78
msaeiid <mskarbaschian@gmail.com>
2024-03-07 20:44:08 +0330

    UpdateAPIView & DestroyAPIView

--- products/views.py
+++ products/views.py
@@ -23,31 +23,25 @@ class ProductListCreateAPIViews(generics.ListCreateAPIView):
         # instead above send a django signal is possible
 
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
+class ProductUpdateAPIViews(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        product = serializer.save()
+        if not product.content:
+            product.content = product.title
+            product.save()
+
+
+class ProductDestroyAPIViews(generics.DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'pk'
 
 
-@api_view(http_method_names=['POST', 'GET'])
-def product_alt_view(request: Request, pk=None, *args, **kwargs):
-    if request.method == "GET":
-        products = get_list_or_404(Product)
-        if pk:
-            product = get_object_or_404(Product, pk=pk)
-            serializer = ProductSerializer(product, many=False)
-        else:
-            serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    elif request.method == "POST":
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            title = serializer.validated_data['title']
-            content = serializer.validated_data['content']
-            if content:
-                content = title
-                serializer.save(content=content)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    # this is why generic views are good
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
