b046b87f49493225d72f6f3ceadecab236ca79a9
msaeiid <mskarbaschian@gmail.com>
2024-03-08 00:40:45 +0330

    Custom Permissions

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('<int:pk>/', views.ProductMixinView.as_view(), name='detail_view'),
-    path('<int:pk>/update', views.ProductUpdateAPIViews.as_view(), name='update_view'),
-    path('<int:pk>/destroy', views.ProductDestroyAPIViews.as_view(), name='destroy_view'),
+    # path('<int:pk>/update', views.ProductUpdateAPIViews.as_view(), name='update_view'),
+    # path('<int:pk>/destroy', views.ProductDestroyAPIViews.as_view(), name='destroy_view'),
     path('', views.ProductMixinView.as_view(), name='create_view'),
 ]
