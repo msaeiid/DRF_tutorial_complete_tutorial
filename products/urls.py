7c00b3a3fe7181a80e2c6fedffc4a529a5acb78d
msaeiid <mskarbaschian@gmail.com>
2024-03-07 13:34:17 +0330

    Django Rest Framework Generics CreateAPIView

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls.conf import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='detail_view')
+    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='detail_view'),
+    path('', views.ProductCreateAPIViews.as_view(), name='create_view')
 ]
--- products/views.py
+++ products/views.py
@@ -4,6 +4,21 @@ from .serializers import ProductSerializer
 from products.models import Product
 
 
+class ProductCreateAPIViews(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data['title']
+        content = serializer.validated_data['content']
+        if content:
+            content = title
+            serializer.save(content=content)
+        # simple way to add aditional data to serializer
+        # instead above send a django signal is possible
+
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
